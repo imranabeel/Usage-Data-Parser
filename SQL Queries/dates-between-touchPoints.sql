a464bac97d6a248bdd5cf674fa68debea00ef01a
Jonathan Raines <jonathanraines@openbionics.com>
2019-12-16 21:51:26 +0000

    Added metres traveled to usage data analysis

--- SQL Queries/dates-between-touchPoints.sql
+++ SQL Queries/dates-between-touchPoints.sql
@@ -1,6 +1,6 @@
-DROP TABLE #temp1
-DROP TABLE #temp2
-DROP TABLE #temp3
+DROP TABLE IF EXISTS #temp1
+DROP TABLE IF EXISTS #temp2
+DROP TABLE IF EXISTS #temp3
 SELECT
 	TouchPointID,
 	HandNumber,
@@ -11,7 +11,7 @@ SELECT
 		Date)
 	AS [Number of Days]
 INTO #temp1
-FROM touchPoints 
+FROM HeroUsageData.dbo.touchPoints 
 ORDER BY HandNumber, Date
 
 SELECT
@@ -19,7 +19,7 @@ SELECT
 	SUM(DATEDIFF(second, '0:00:00', onTime)) AS [Total On Time (s)],
 	SUM(DATEDIFF(second, '0:00:00', activeTime)) AS [Total Active Time (s)]
 	INTO #temp2
-FROM sessions GROUP BY CollectedInTouchPoint
+FROM HeroUsageData.dbo.sessions GROUP BY CollectedInTouchPoint
 
 SELECT
 	a.TouchPointID,
@@ -28,18 +28,25 @@ SELECT
 	a.[Number of Days], 
 	b.[Total On Time (s)],
 	b.[Total Active Time (s)],
+	CAST(b.[Total Active Time (s)] as float) * 23 / 1000 AS [Metres Traveled],
 	CAST(b.[Total On Time (s)] AS float)/CAST(a.[Number of Days] AS float) AS [Average On Time Per Day (s)],
 	CAST(b.[Total Active Time (s)] AS float)/CAST(a.[Number of Days] AS float) AS [Average Active Time Per Day (s)]
 INTO #temp3
 FROM #temp1 a JOIN #temp2 b ON a.TouchPointID=b.CollectedInTouchPoint
 
+/*
 SELECT * FROM #temp1
 SELECT * FROM #temp2
-SELECT * FROM #temp3
+SELECT * FROM #temp3 
+*/
 
 SELECT
 	HandNumber,
+	SUM([Total On Time (s)]) AS [Total On Time (s)],
+	SUM([Total Active Time (s)]) AS [Total Active Time (s)],
+	SUM([Metres Traveled]),
 	CONVERT(DECIMAL(10,2), AVG([Average On Time Per Day (s)])) AS [Average On Time Per Day (s)],
-	CONVERT(DECIMAL(10,2),AVG([Average Active Time Per Day (s)])) AS [Average Active) Time Per Day (s)]
+	CONVERT(DECIMAL(10,2), AVG([Average Active Time Per Day (s)])) AS [Average Active Time Per Day (s)],
+	CONVERT(DECIMAL(10,2), AVG([Metres Traveled])) AS [Average Distance Traveled Per Day (m)]
 FROM #temp3
 GROUP BY HandNumber
