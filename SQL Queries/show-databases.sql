d06d24a9e8fbdad267ed4618cde478c3739242b4
JonathanRaines <jonathanraines@openbionics.com>
2019-12-20 10:46:12 +0000

    Changed SQL query to run from anywhere
    Added full path to database

--- SQL Queries/show-databases.sql
+++ SQL Queries/show-databases.sql
@@ -1,2 +1,2 @@
-SELECT * FROM touchPoints ORDER BY Date
-SELECT * FROM sessions ORDER BY CollectedInTouchPoint, SessionNumber
+SELECT * FROM HeroUsageData.dbo.touchPoints ORDER BY Date
+SELECT * FROM HeroUsageData.dbo.sessions ORDER BY CollectedInTouchPoint, SessionNumber
